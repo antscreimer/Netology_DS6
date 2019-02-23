e52989de733bbfec404380964d89c1e34274809e
antscreimer <44849260+antscreimer@users.noreply.github.com>
2019-02-23 12:22:32 +0300

    Update Final_work_SQL.sql

--- HWK/Module 1/Final_work_SQL.sql
+++ HWK/Module 1/Final_work_SQL.sql
@@ -3,20 +3,31 @@ CREATE TABLE wagon_repair (
   wagon_id INTEGER,
   depot_id INTEGER,
   repair_type VARCHAR(100),
-  cost
+  cost numeric(8,2)
   );
   
 CREATE TABLE wagon (
   wagon_id INTEGER PRIMARY KEY,
   wagon_type VARCHAR(100)
   );
-  
+INSERT INTO wagon VALUES
+(1, 'Open wagon'),-- Полувагон
+(2, 'Flat wagon'),-- Платформа
+(3, 'Covered wagon'),-- Крытый вагон
+(4, 'Tank wagon');-- Цистерна
+
 CREATE TABLE detail (
   detail_id INTEGER PRIMARY KEY,
   wagon_id INTEGER,
-  detail_name
+  detail_name VARCHAR
  );
- 
+
+INSERT INTO detail VALUES
+(1, 'wheelset'), -- колесная пара
+(2, 'truck bolster'), -- надрессорная балка
+(3, 'side frame'), -- боковая рама
+(4, 'coupler'); -- тяговый хомут
+
 CREATE TABLE depot (
   ID INTEGER,
   depot_id INTEGER,
@@ -24,7 +35,3 @@ CREATE TABLE depot (
   depot_name VARCHAR(100),
   detail_quantity INTEGER
  );
-
-CREATE TABLE warehousing (
-  detail_id
-  depot
