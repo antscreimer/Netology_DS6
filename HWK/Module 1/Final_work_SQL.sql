a1ac05ee257beb7811c1914cae6e4ad80ee2f9dc
antscreimer <44849260+antscreimer@users.noreply.github.com>
2019-02-25 09:45:55 +0300

    Update Final_work_SQL.sql

--- HWK/Module 1/Final_work_SQL.sql
+++ HWK/Module 1/Final_work_SQL.sql
@@ -1,12 +1,70 @@
+-- Создание таблицы описания грузового вагона
+
+CREATE TABLE wagon (
+  wagon_id INTEGER PRIMARY KEY,
+  wagon_type VARCHAR(100)
+  );
+INSERT INTO wagon VALUES
+('1', 'Open wagon'),-- Полувагон
+('2', 'Flat wagon'),-- Платформа
+('3', 'Covered wagon'),-- Крытый вагон
+('4', 'Tank wagon');-- Цистерна
+
+-- Создание таблицы деталей вагона
+
+CREATE TABLE detail (
+  detail_id INTEGER PRIMARY KEY,
+  -- wagon_id INTEGER,
+  detail_name VARCHAR
+ );
+
+INSERT INTO detail VALUES
+('1', 'wheelset'), -- колесная пара
+('2', 'truck bolster'), -- надрессорная балка
+('3', 'side frame'), -- боковая рама
+('4', 'coupler'); -- тяговый хомут
+
+-- Создание таблицы депо
+
+CREATE TABLE depot (
+  ID INTEGER,
+  depot_id INTEGER PRIMARY KEY,
+  detail_id INTEGER REFERENCES detail(detail_id),
+  detail_quantity INTEGER
+ );
+
+INSERT INTO depot VALUES
+('1', 1, 1, 20),
+('2', 1, 2, 40),
+('3', 1, 3, 25),
+('4', 1, 4, 15),
+('5', 2, 1, 15),
+('6', 2, 2, 16),
+('7', 2, 3, 17),
+('8', 3, 1, 18),
+('9', 3, 2, 20),
+('10', 3, 4, 17),
+('11', 4, 3, 21),
+('12', 4, 4, 25),
+('13', 5, 1, 30),
+('14', 5, 2, 15),
+('15', 5, 3, 8),
+('16', 5, 4, 11), 
+('17', 6, 2, 19),
+('18', 6, 3, 15),
+('19', 6, 4, 14);
+
+-- Создание таблицы ремонтов
+
 CREATE TABLE wagon_repair (
   repair_id INTEGER,
-  wagon_id INTEGER,
-  numb INTEGER,
-  detail_id INTEGER,
+  wagon_id INTEGER REFERENCES wagon(wagon_id),
+  numb VARCHAR(10),
+  detail_id INTEGER REFERENCES detail(detail_id),
   detail_number INTEGER,
-  depot_id INTEGER,
+  depot_id INTEGER REFERENCES depot(depot_id),
   repair_type VARCHAR(100),
-  price numeric(8,2)
+  price numeric
   );
 
 INSERT INTO wagon_repair VALUES
@@ -42,68 +100,3 @@ INSERT INTO wagon_repair VALUES
 ('30', '4', '6t', '2', 2, '6', 'покупка', 40000),
 ('31', '4', '7t', '1', 1, '6', 'покупка', 80000),
 ('32', '4', '7t', '2', 2, '6', 'замена', 20000);
-  
-CREATE TABLE wagon (
-  wagon_id INTEGER PRIMARY KEY,
-  wagon_type VARCHAR(100)
-  );
-INSERT INTO wagon VALUES
-('1', 'Open wagon'),-- Полувагон
-('2', 'Flat wagon'),-- Платформа
-('3', 'Covered wagon'),-- Крытый вагон
-('4', 'Tank wagon');-- Цистерна
-
-CREATE TABLE detail (
-  detail_id INTEGER PRIMARY KEY,
-  wagon_id INTEGER,
-  detail_name VARCHAR
- );
-
-INSERT INTO detail VALUES
-('1', 'wheelset'), -- колесная пара
-('2', 'truck bolster'), -- надрессорная балка
-('3', 'side frame'), -- боковая рама
-('4', 'coupler'); -- тяговый хомут
-
-
-CREATE TABLE depot_name (
-  ID INTEGER,
-  depot_name VARCHAR (100)
-);
-
-INSERT INTO depot_name VALUES
-('1', 'central_depot'),
-('2', 'north_depot'),
-('3', 'south_depot'),
-('4', 'east_depot'),
-('5', 'west_depot'),
-('6', 'reserve_depot');
-
-
-CREATE TABLE depot (
-  ID INTEGER,
-  depot_id INTEGER,
-  detail_id INTEGER,
-  detail_quantity INTEGER
- );
-
-INSERT INTO depot VALUES
-('1', 1, 1, 20),
-('2', 1, 2, 40),
-('3', 1, 3, 25),
-('4', 1, 4, 15),
-('5', 2, 1, 15),
-('6', 2, 2, 16),
-('7', 2, 3, 17),
-('8', 3, 1, 18),
-('9', 3, 2, 20),
-('10', 3, 4, 17),
-('11', 4, 3, 21),
-('12', 4, 4, 25),
-('13', 5, 1, 30),
-('14', 5, 2, 15),
-('15', 5, 3, 8),
-('16', 5, 4, 11), 
-('17', 6, 2, 19),
-('18', 6, 3, 15),
-('19', 6, 4, 14);
